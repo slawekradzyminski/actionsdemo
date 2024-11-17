37e3c2ced9fdf7a8de38201f761fc33c01a0cee1
slawekradzyminski <slawomir.radzyminski@gmail.com>
2024-11-17 09:48:06 +0100

    allure report

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,14 @@
 const { defineConfig } = require("cypress");
+const { allureCypress } = require("allure-cypress/reporter");
 
 module.exports = defineConfig({
   projectId: "chnbdd",
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      allureCypress(on, config, {
+        resultsDir: "allure-results",
+      });
+      return config;
     },
   },
 });
