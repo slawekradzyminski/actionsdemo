c94d30d766510ceb77de167b0bb11e2bf4828c73
slawekradzyminski <slawomir.radzyminski@gmail.com>
2024-11-17 12:50:58 +0100

    add tests for localstack

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,29 @@
+/// <reference types="cypress" />
+
+describe('Localstack login tests', () => {
+    beforeEach(() => {
+      cy.visit('http://localhost:8081')
+    })
+  
+    it('should successfully login', () => {
+        // when
+        cy.get('input[name="username"]').type('admin')
+        cy.get('input[name="password"]').type('admin')
+        cy.get('.btn-primary').click()
+
+        // then
+        cy.get('h1').should('contain.text', 'Hi Slawomir')
+    })
+
+    it('should fail login', () => {
+        // when
+        cy.get('input[name="username"]').type('admin')
+        cy.get('input[name="password"]').type('wrong')
+        cy.get('.btn-primary').click()
+
+        // then
+        cy.get('.alert-danger').should('have.text', 'Invalid username/password supplied')
+    })
+  
+  })
+  
